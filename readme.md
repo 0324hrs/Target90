e5c5c86f49e181b48c7f4546fd64ed1fa5e65016
GacheruJr <mke445571@gmail.com>
2022-05-30 21:07:24 +0300

    tippy python

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
+##### target90 python
 run the code in your IDE to check the days,months,weeks and years you have left to hit 90 years😂😂😂😂
+##### tippy python
+a simple python program to calculate tips and bills paid among members of a group
---
+++ tippy.py
@@ -0,0 +1,15 @@
+print("welcome to tippy!!")
+
+# 
+bill = float(input("what is your total bill? \n ksh"))
+
+tip = int(input("how much tip do you want to give? 10, 15 \n"))
+
+people = int(input("how many people to split the bill? \n"))
+# 
+tip_percentage = tip/ 100
+tt_tip_amount = tip_percentage * bill
+
+total_billed = bill + tt_tip_amount
+# 
+print(total_billed)
